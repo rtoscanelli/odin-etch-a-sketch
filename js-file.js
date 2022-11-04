e7a4e570b4d464ebb29c665b4d0c3e1da9d9d0d2
rtoscanelli <ricardotoscanelli@gmail.com>
2022-11-04 20:18:01 +0000

    Add hover in grid

--- js-file.js
+++ js-file.js
@@ -1,4 +1,7 @@
 const board = document.querySelector('div.board');
+const body = document.querySelector('body');
+
+let pixels = [];
 
 for (let i = 0; i < 16**2; i++) {
         const pixel = document.createElement('div');
@@ -7,4 +10,25 @@ for (let i = 0; i < 16**2; i++) {
         pixel.style.height = '60px';
         pixel.style.border = 'solid red 1px';
         board.appendChild(pixel);
+        pixels.push(pixel);
 }
+
+const pixel = document.createElement('div');
+pixel.style.boxSizing = 'border-box'; //make widht and height with border
+pixel.style.width = '60px';
+pixel.style.height = '60px';
+pixel.style.border = 'solid red 1px';
+body.appendChild(pixel);
+
+pixels.forEach(pixel => {
+        
+        pixel.addEventListener('mouseover', () => {
+                let color = pixel.style.backgroundColor;
+                if (color == 'blue') {
+                        pixel.style.backgroundColor = 'white';
+                }
+                else {
+                        pixel.style.backgroundColor = 'blue'
+                }
+        });
+});
