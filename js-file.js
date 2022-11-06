21c44c8b49ccde7ffb66dbeb16f7c3a07e6d93b8
rtoscanelli <ricardotoscanelli@gmail.com>
2022-11-06 20:18:03 +0000

    Reset board with many inputs

--- js-file.js
+++ js-file.js
@@ -1,9 +1,6 @@
 const board = document.querySelector('.board');
 const modifyGridButton = document.querySelector('.modify-grid');
-const p = document.querySelector('p');
-console.log(getComputedStyle(p).color);
 
-let pixels = [];
 
 // for (let i = 0; i < 16**2; i++) {
 //         const pixel = document.createElement('div');
@@ -15,10 +12,11 @@ let pixels = [];
 //         pixels.push(pixel);
 // }
 
+let pixels = [];
+
 pixels.forEach(pixel => {pixel.addEventListener('mouseover', changeColor)});
 
 modifyGridButton.addEventListener('click', createBoard);
-
 
 function createBoard() {
         pixels = [];
@@ -30,9 +28,9 @@ function createBoard() {
                 pixel.style.boxSizing = 'border-box'; //make widht and height with border
                 pixel.style.width = pixel.style.height = `${boardDim / gridSize}px`;
                 pixel.style.border = 'solid red 1px';
-                board.appendChild(pixel);
                 pixels.push(pixel);
         }
+        board.replaceChildren(...pixels);
         
         pixels.forEach(pixel => {pixel.addEventListener('mouseover', changeColor)});
 }
