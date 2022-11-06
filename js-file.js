ebeeb95e40f2db3a375a6b0487b692fb08534583
rtoscanelli <ricardotoscanelli@gmail.com>
2022-11-06 20:24:44 +0000

    Add a default board

--- js-file.js
+++ js-file.js
@@ -1,18 +1,17 @@
 const board = document.querySelector('.board');
 const modifyGridButton = document.querySelector('.modify-grid');
 
-
-// for (let i = 0; i < 16**2; i++) {
-//         const pixel = document.createElement('div');
-//         pixel.style.boxSizing = 'border-box'; //make widht and height with border
-//         pixel.style.width = '60px';
-//         pixel.style.height = '60px';
-//         pixel.style.border = 'solid red 1px';
-//         board.appendChild(pixel);
-//         pixels.push(pixel);
-// }
-
 let pixels = [];
+
+for (let i = 0; i < 16**2; i++) {
+        const pixel = document.createElement('div');
+        pixel.style.boxSizing = 'border-box'; //make widht and height with border
+        pixel.style.width = '60px';
+        pixel.style.height = '60px';
+        pixel.style.border = 'solid red 1px';
+        board.appendChild(pixel);
+        pixels.push(pixel);
+}
 
 pixels.forEach(pixel => {pixel.addEventListener('mouseover', changeColor)});
 
